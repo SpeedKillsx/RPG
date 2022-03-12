0a837b682757791bcfa9ac86fca98e8477d2132a
SpeedsKillsx <amayaslabchri88@gmail.com>
2022-03-12 17:34:38 +0100

    Weapon animation

--- Assets/Script/EnemyHitBox.cs
+++ Assets/Script/EnemyHitBox.cs
@@ -10,7 +10,7 @@ public class EnemyHitBox : Collidable
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.tag == "Fighter" && coll.name == "Player")
-        {
+        {   // Create a damage and send it to player
             Damage dmg = new Damage
             {
                 damageAmount = damage,
